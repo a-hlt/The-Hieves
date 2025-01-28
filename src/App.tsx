1c615006b772644c35f2715349cd049c5bee9367
NotGrafik <pablo.jeanlouis@ynov.com>
2025-01-28 13:32:40 +0100

    fix(App): remove unused Map component import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Map from './components/Map'
 import Interface from './components/Interface'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
