a7770c80b6ad98682de8369f67c3356ba7ed0f3e
KillianFra <110053182+KillianFra@users.noreply.github.com>
2025-01-28 14:53:55 +0100

    fix(merge): fixed merge

--- src/components/Interface.tsx
+++ src/components/Interface.tsx
@@ -5,21 +5,17 @@ import Navbar from "./Navbar";
 function Interface() {
     return (
         <div className="h-screen w-screen bg-gray-200/50 flex flex-col">
-            <div className="mx-5 my-2 bg-white rounded-xl flex items-center text-black text-xl font-bold">
+            <div className="mx-5 mt-4 bg-white rounded-xl shadow-custom flex items-center text-black text-xl font-bold">
                 <Navbar />
             </div>
-            <div className="flex flex-grow px-5 gap-5">
-                <div className="flex-grow">
-                    <div className="flex flex-grow p-5 gap-5">
-                        <div className="flex-grow shadow-custom rounded-xl">
-                            <Map />
-                        </div>
-                        
-                        {/* Sidebar */}
-                        <div className="w-1/4 bg-yellow-500 rounded-xl shadow-custom">
-                            <ChatPage />
-                        </div>
-                    </div>
+            <div className="flex flex-grow p-5 gap-5">
+                <div className="flex-grow shadow-custom rounded-xl">
+                    <Map />
+                </div>
+
+                {/* Sidebar */}
+                <div className="w-1/4 bg-yellow-500 rounded-xl shadow-custom">
+                    <ChatPage />
                 </div>
             </div>
         </div>
