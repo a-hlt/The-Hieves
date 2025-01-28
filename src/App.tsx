2a1094acb6a1981869fcc30969e9c644e87bf4e4
Madjinda Lucas <lucasmadjinda@gmail.com>
2025-01-28 11:35:39 +0100

    app remis par défaut pour éviter conflits

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,28 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "./components/ui/button";
-import ChatPage from "./pages/chat/chatPage.tsx";
+
 function App() {
+	const [count, setCount] = useState(0);
+
 	return (
 		<>
-			<ChatPage />
+			<div>
+				<a href="https://vite.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1 className="text-blue-500 text-3xl font-bold">Vite + React</h1>
+			<div className="card">
+				<button className="bg-green-500 p-2 rounded" onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<Button className="">Hello world!</Button>
+			</div>
+			<p className="read-the-docs text-purple-500">Click on the Vite and React logos to learn more</p>
 		</>
 	);
 }
